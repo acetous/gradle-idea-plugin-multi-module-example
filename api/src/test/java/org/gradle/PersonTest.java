d8d17acb1f5380dc576900228e0f9af62db2a323
Sebastian Herbermann <sebastian.herbermann@googlemail.com>
2018-09-07 14:59:06 +0200

    add testcase

--- api/src/test/java/org/gradle/PersonTest.java
+++ api/src/test/java/org/gradle/PersonTest.java
@@ -3,6 +3,10 @@ package org.gradle;
 import org.junit.Test;
 
 public class PersonTest {
+
+    // this should not compile becauhse HelperTest is located in the shared-module.
+    private HelperTest helperTest = new HelperTest();
+
     @Test
     public void ok() {
     }
